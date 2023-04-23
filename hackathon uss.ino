c6775bd730bb98e775d0eb2a10ceecc105b25c2c
Okto3 <73514013+Okto3@users.noreply.github.com>
2023-04-23 15:11:44 +1000

    Add files via upload

--- hackathon uss.ino
+++ hackathon uss.ino
@@ -1,28 +1,31 @@
 const int trigPin = 9;
 const int echoPin = 10;
 int time = 0;
-// defines variables
+
 long duration;
 int distance;
 void setup() {
-  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
-  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
   pinMode(2, OUTPUT);
-  Serial.begin(9600); // Starts the serial communication
+  Serial.begin(9600);
 }
 void loop() {
-  // Clears the trigPin
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
-  // Sets the trigPin on HIGH state for 10 micro seconds
+  // Sets the trigPin on HIGH state for 10 microseconds
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
   // Reads the echoPin, returns the sound wave travel time in microseconds
   duration = pulseIn(echoPin, HIGH);
-  // Calculating the distance
+  // Calculating the distance based on the speed of sound
   distance = duration * 0.034 / 2;
+
+  // blinks LED at a frequency proportional to the distance
+  // only starts flashing once it its <1m away
   if(distance < 100) {
+    // distance is measured in cm, so for a distance of 1m, it will blink on for 1s and off for 1s
     time = 10 * distance;
     digitalWrite(2,HIGH);
     delay(time);
--- hackathon.ino
+++ hackathon.ino
@@ -46,10 +46,10 @@ void setup() {
   pinMode(leftLED, OUTPUT);
   pinMode(rightLED, OUTPUT);
 
-  pinMode(trigPin, OUTPUT); // set the trigger pin as an output
+  pinMode(trigPin, OUTPUT); 
   pinMode(echoPin, INPUT);
 
-
+  //perform a calibration to know that it starts in the centre
   delay(1000);
   Serial.println("Calibrating:");
   calibrate();
@@ -61,18 +61,19 @@ void setup() {
 void calibrate() {
   digitalWrite(dirPin, LOW);
   delay(1);
-  //moveToEndLimit();
+  //moves until a limit switch is pressed
   while(digitalRead(9) != 1){
     digitalWrite(stepPin, HIGH);
     delay(1);
     digitalWrite(stepPin, LOW);
     delay(1);
   }
+  // sets the minimum pos
   minPos = analogRead(A0);
   Serial.println("finished first cal");
   digitalWrite(dirPin, HIGH);
   delay(1);
-  //moveToEndLimit();
+  //moves to the other end limit;
   while(digitalRead(10) != 0){
     digitalWrite(stepPin, HIGH);
     delay(1);
@@ -80,12 +81,14 @@ void calibrate() {
     delay(1);
   }
   maxPos = analogRead(A0);
+  // calculates the average between the max and min
   middlePosition = (maxPos + minPos)/2;
   Serial.println("midle pos:");
   Serial.println(middlePosition);
   Serial.println(minPos);
   Serial.println(maxPos);
   digitalWrite(dirPin, LOW);
+  // moves halfway in between
   stepForward(1500, 800);
   delay(2000);
 
@@ -94,52 +97,58 @@ void calibrate() {
 
 
 void loop() {
-
+  // checks if the left limit switch is pressed, on only releases if the angle is such that it will correct itself
   while(digitalRead(9) == 1 and analogRead(A0) > middlePosition){
     Serial.println("left is pressed");
+    // turns on an led indicating that user intevention is required
     digitalWrite(leftLED, HIGH);
     delay(5);
   }
-
+  // checks the other limit switch
   while(digitalRead(10) == 0 and analogRead(A0) < middlePosition){
     Serial.println("right is pressed");
+    // turns on the other led
     digitalWrite(rightLED, HIGH);
     delay(5);
   }
-
+  // turns the LEDs off
   digitalWrite(leftLED, LOW);
   digitalWrite(rightLED, LOW);
 
+  // read the position of the potentiometer
   int pot = analogRead(A0);
   
-
-  int error = pot - middlePosition; // + (analogRead(A1)-512)/20;
+  // calculates the error between the middle position and where it is
+  int error = pot - middlePosition
   //Serial.println(steps);
   if(abs(error) < 5) {
-    //in stable equilibrium
-    
+    //in stable equilibrium, amountToKick is how much to move it to get it out of an equilibrium and move it in the correct direction
     amountToKick = KpKick * steps * -1; 
-    
   }
   else{
     amountToKick = 0;
   }
-
+  // this is the basic control loop thats just a proportional controller + the kick which it really doesn't like
   amountToMove = error * Kp + amountToKick; // + (Kd * (error - errorPrev) / dt);
 
+  //keep track of how far it's moved
   steps = steps + amountToMove;
 
+  //this code actually moves it
   if(amountToMove > 0){
+    // sets the direction and amount to move
     digitalWrite(dirPin, 0);
     stepForward(amountToMove, 600);
   }
   else{
+    //moves it the other way, making the amount to move positive
     digitalWrite(dirPin, 1);
     stepForward(-1*amountToMove, 600);
   }  
 }
 
 void stepForward (int steps, int delayTime) {
+  // this function moves the steppers
   for(int i=0; i<steps; i++){
     
     digitalWrite(stepPin, HIGH);
@@ -147,6 +156,7 @@ void stepForward (int steps, int delayTime) {
     digitalWrite(stepPin, LOW);
     delayMicroseconds(delayTime);
     if(amountToMove > 0){
+      // tracks how far it's gone
       amountToMove--;
     }
     else{
